3ec366aba0f42284d659fcc8e1ebc1df685803b0
FelopaterA <felopater@localhost.localdomain>
2025-02-24 15:56:33 +0200

    lab

---
+++ day1.py
@@ -0,0 +1,86 @@
+# fullname =input()
+# numOfLet =0
+# for let in fullname:
+#      if (let == "a" or let == "e" or let == "o" or let == "i" or let == "u"):
+#         numOfLet =numOfLet+1
+# print(numOfLet)
+
+
+#2
+
+# def numarrya(len,start):
+#     count=1
+#     while count <=len:
+#      print(start)
+#      start=start+1
+#      count=count+1
+
+
+# numarrya(2,10)
+
+#3
+# arrayOfInt =input()
+# x= sorted(arrayOfInt,reverse=True)
+# print(x)
+#--------
+# arrayOfInt =input()
+# x= sorted(arrayOfInt,reverse=False)
+# print(x)
+
+
+#4
+# num =int(input())
+
+# def fizzbuzz(x):
+#     if x % 3 == 0 and x % 5 == 0:
+#         print("FizzBuzz")
+#     elif x % 3 == 0:
+#         print("Fizz")
+#     elif x % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(x)
+
+# fizzbuzz(num)
+
+
+#5
+
+# w=input()
+# def onewords (y):
+#     reversed= y[::-1]
+#     print(reversed)
+
+# onewords(w)
+
+#--------6
+
+
+# 
+
+#-------7
+# def userinfo():
+#     name =input("Enter your Name: ")
+#     if name.isalpha():
+     
+#      email =input("Enter your email:")
+#      print(name , email)
+
+#     else:
+#         print("wrong name")
+#         exit
+
+# userinfo()
+
+#---------8
+# tex ="hi in iti welcome in iti "
+# def occurs(text):
+#    tex=text.count("iti")
+#    print(tex)
+
+
+
+# occurs(tex)
+
+
+#9
